048d28019839c395bee40b91242a4648df495e2e
Phati <phatisavant@gmail.com>
2024-05-14 19:05:43 +0530

    added spring boot project

---
+++ src/test/java/io/github/phati/jenkinspipeline/JenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.phati.jenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
