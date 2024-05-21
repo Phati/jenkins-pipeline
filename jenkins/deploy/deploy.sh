d9ba4af906a83c1e5defa1385d532482093aa5ee
Phati Sawant <61258100+Phati@users.noreply.github.com>
2024-05-22 01:46:54 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -14,6 +14,9 @@ docker login -u phatijava -p $DOCKER_HUB_PASSWORD
 echo "*** Pull docker image image ***"
 docker pull phatijava/$IMAGE:$RELEASE_VERSION
 
+docker kill test
+docker rm test
+
 
 echo "*** Run image ***"
 docker run -d --name test -p $DOCKER_PORT:$DOCKER_PORT  phatijava/$IMAGE:$RELEASE_VERSION
