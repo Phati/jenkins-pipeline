ca49b22d47a9254c5a7193f798d993170a8fb8ba
Phati <phatisavant@gmail.com>
2024-05-22 01:53:28 +0530

    added new api to test Jenkins build

--- src/main/java/io/github/phati/jenkinspipeline/controller/TestController.java
+++ src/main/java/io/github/phati/jenkinspipeline/controller/TestController.java
@@ -1,10 +1,14 @@
 package io.github.phati.jenkinspipeline.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @NoArgsConstructor
@@ -12,6 +16,15 @@ public class TestController  {
     @GetMapping("/test")
     public ResponseEntity<String> getMessage(){
         return new ResponseEntity<>("Hello from test api",HttpStatus.OK);
+    }
+
+
+    @GetMapping("/test2")
+    public ResponseEntity<Map<String,Object>> test(){
+        Map<String,Object> data = new HashMap<>();
+        data.put("app_name","Spring boot app");
+        data.put("version","3.0.3");
+        return new ResponseEntity<>(data,HttpStatus.OK);
     }
 
 }
