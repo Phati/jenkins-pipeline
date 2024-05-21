fa850eb9700bfcca7d452554e217e85de731eb96
Phati <phatisavant@gmail.com>
2024-05-21 10:24:31 +0530

    add workspace path in scripts

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,6 @@ echo "*******************************"
 echo "***** Running Unit Tests ******"
 echo "*******************************"
 
+WORKSPACE=/var/lib/jenkins/workspace/spring-boot-app
 
-docker run --rm  -v $PWD:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@"
+docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@"
