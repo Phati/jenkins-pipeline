18be94db886e4a02f97178197703f1a10ed92d79
Phati Sawant <61258100+Phati@users.noreply.github.com>
2024-06-02 00:11:14 +0530

    Update mvn.sh

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -5,5 +5,5 @@ echo "***** Running Unit Tests ******"
 echo "*******************************"
 
 WORKSPACE=/var/lib/jenkins/workspace/spring-boot-app
-
+chmod 777 /var/run/docker.sock
 docker run --rm  -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3.8.3-openjdk-17 "$@"
